d97bace9ff7dd1f857105f10c65587d9ea938925
ttmuangkeo <tim.muangkeo@gmail.com>
2018-05-24 20:47:52 -0700

    added class/accounts to call functions in register file

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -19,8 +19,11 @@ class Account {
 
 	private function validateUsername($un) {
 		if(strlen($un) > 25 || strlen($un) < 5) {
-
+			array_push($this->$errorArray, 'Your username must be between 5 and 25 characters');
+			return;
 		}
+
+		//to do: check if username exist 
 
 	}
 
