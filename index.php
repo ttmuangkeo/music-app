70f1d31d3598301c40605a1aeb0a629c1f131c00
ttmuangkeo <tim.muangkeo@gmail.com>
2018-05-24 19:55:31 -0700

    created register form:

--- index.php
+++ index.php
@@ -17,6 +17,42 @@
 		</p>
 		<button type="submit" name="loginButton">LOG IN</button>
 	</form>
+
+
+	<form id="registerForm" action="register.php" method="POST">
+		<h2>Create your free account</h2>
+		<p>
+			<label for="username">Username</label>
+			<input id="username" name="username" type="text" placeholder="e.g. ttmuangkeo" required>
+		<p>
+
+		<p>
+			<label for="firstName">First Name</label>
+			<input id="firstName" name="firstName" type="text" placeholder="e.g. Tim" required>
+		<p>
+		<p>
+			<label for="lastName">Last Name</label>
+			<input id="lastName" name="lastName" type="text" placeholder="e.g. Muangkeo" required>
+		<p>
+		<p>
+			<label for="email">Email</label>
+			<input id="email" name="email" type="email" placeholder="e.g. ttmuangkeo@gmail.com" required>
+		<p>
+		<p>
+			<label for="email2">Confirm email</label>
+			<input id="email2" name="email2" type="email" placeholder="e.g. ttmuankgeo@gmail.com" required>
+		<p>
+		<p>
+			<label for="password">Password</label>
+			<input id="password" name="password" type="password" required>
+		<p>
+		<p>
+			<label for="password2">Confirm password</label>
+			<input id="password2" name="password2" type="password" required>
+		<p>
+
+		<button type="submit" name="registerButton">Sign Up</button>
+	</form>
 </div>
 </body>
 </html>
