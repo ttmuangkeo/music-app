34854f47d336db1ff2a419cbb55fb4d13ed99ae8
ttmuangkeo <tim.muangkeo@gmail.com>
2018-05-24 19:48:12 -0700

    created log in form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to my Music Project</title>
+</head>
+<body>
+<div id="inputContainer">
+	<form id="loginForm" action="register.php" method="POST">
+		<h2>Login into your account</h2>
+		<p>
+			<label for="loginUsername">Username</label>
+			<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. ttmuangkeo" required>
+		<p>
+		<p>
+			<label for="loginPassword">Password</label>
+			<input id="loginPassword" name="loginPassword" type="password" required>
+		</p>
+		<button type="submit" name="loginButton">LOG IN</button>
+	</form>
+</div>
+</body>
+</html>
