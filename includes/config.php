24cd787deda54af67c1530bd909903aa751a5d2e
ttmuangkeo <tim.muangkeo@gmail.com>
2018-06-10 15:24:31 -0700

    connect to sql database

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php 
+	ob_start();
+
+	$timezone = date_default_timezone_set("America/Los_Angeles");
+
+	$con = mysqli_connect("localhost", "root", "", "musicApp");
+	if(mysqli_connect_errno()) {
+		echo "failed to connect: " . mysqli_connect_errno();
+	}
+?>
